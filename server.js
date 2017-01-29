9ebcf5796cff8c4514ef0ead09ad1c406eea216b
Marco Berube <mberube@Marcos-MacBook-Pro.local>
2017-01-29 15:48:13 -0500

    interval added

--- server.js
+++ server.js
@@ -11,18 +11,21 @@ catch(err){console.log(err); return -1;}
 var lennox_url = 'https://services.myicomfort.com/DBAcessService.svc/GetTStatInfoList?GatewaySN=' + lennox_gatewaysn + '&TempUnit=&Cancel_Away=-1',
     lennox_auth = "Basic " + new Buffer(lennox_username + ":" + lennox_password).toString("base64");
 
-var time = new Date;
-
 var lennox = require('./lennox.js');
-lennox.record(api_url, time, lennox_url,lennox_auth);
-
 var photon = require('./photon.js');
 var token = process.env['TOKEN'];
 
-var device_id = process.env['DEVICE1_ID'],
-    device_var = process.env['DEVICE1_VAR1'];
-photon.record(api_url, time, device_id, device_var, token);
+setInterval(function() {
 
-var device_id = process.env['DEVICE2_ID'],
-    device_var = process.env['DEVICE2_VAR1'];
-photon.record(api_url, time, device_id, device_var, token);
+  var time = new Date;
+  lennox.record(api_url, time, lennox_url,lennox_auth);
+
+  var device_id = process.env['DEVICE1_ID'],
+      device_var = process.env['DEVICE1_VAR1'];
+      photon.record(api_url, time, device_id, device_var, token);
+
+  var device_id = process.env['DEVICE2_ID'],
+      device_var = process.env['DEVICE2_VAR1'];
+      photon.record(api_url, time, device_id, device_var, token);
+      
+}, polling_freq);
